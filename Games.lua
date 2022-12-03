747fb0139ca7353cac4aa025eadbd7bc0e15dca6
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-03 22:00:14 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -23,7 +23,7 @@ local Supported_Games = {
  ["6839171747"] = "DoorsLoader",
  ["6516141723"] = "DoorsLoader",
  ["42829857342"] = "Combat%20Warriors",
- ["155615604"] = "PrisonLife" 	
+ ["155615604"] = "PrisonLife.lua" 	
 }
 
 local success, err = pcall(function()
