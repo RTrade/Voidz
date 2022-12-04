202778a944a26170c9c83cb0c5abc6ddc83b5010
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-04 10:35:35 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -22,7 +22,7 @@ function CreateNotification(Name,Content,Time,Purpose)
 local Supported_Games = {
  ["6839171747"] = "DoorsLoader",
  ["6516141723"] = "DoorsLoader",
- ["42829857342"] = "Combat%20Warriors",
+ ["4282985734"] = "Combat%20Warriors",
  ["155615604"] = "PrisonLife.lua" 	
 }
 
