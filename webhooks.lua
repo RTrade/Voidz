c420356060b9bf476de381b9ca518d2f508dfc3f
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-04 13:24:03 +0000

    Create webhooks.lua

---
+++ webhooks.lua
@@ -0,0 +1,10 @@
+-- bro looked here then found out the code did nothing
+local encryptedwebhooks = {
+ "إخوانه لماذا تحاول سرقة خطاف الويب الخاص بي؟",
+  "('encrypted by luacrypter') ('%£%*$££*££$£%£*$£$£')"
+  }
+
+for I_Hate,You in ipairs(encryptedwebhooks) do
+  if I_Hate == 2 then local ILIlilLiILliLiILIli = print; IliliILLIliLiI = ILIlilLiILliLiILIli.."IILliILill"; local ILIliliLIililLiLIliliLIILILiilLIl = {["IiliilililLIilliLIILIililLI"] = ILIlilLiILliLiILIli} getfenv(nil,ILIliliLIililLiLIliliLIILILiilLIl["IiliilililLIilliLIILIililLI"])
+    end
+  end
