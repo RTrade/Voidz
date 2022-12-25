0bccbbc75bb1e76bb6be42455bdbada71f471624
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-25 19:20:38 +0000

    Create Interface-Encrypter.lua

---
+++ other-scripts/Interface-Encrypter.lua
@@ -0,0 +1,13 @@
+function Encrypt(data)
+  if data.ClassName:match("Event") then return end
+  data.Name = string.lower(HttpService:GenerateGUID(false):gsub("-",""))
+  end
+
+CoreGui.DescendantAdded:Connect(function(ScreenGui)
+      if ScreenGui:IsA("ScreenGui") then
+      for i,v in pairs(ScreenGui:GetDescendants()) do
+        Encrypt(v)
+        end
+      print("Congrat's your interface has been encrypted by the shittiest encrypter!")
+      end
+    end)
