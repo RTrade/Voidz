d8740cdb1f4fbc52f3d74f8fcdf20374d7eb8446
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-26 14:29:17 +0000

    Create Anti-Damage (swords).lua

---
+++ other-scripts/Anti-Damage (swords).lua
@@ -0,0 +1,6 @@
+local Character = game:GetService("Players").LocalPlayer.Character
+
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+
+HumanoidRootPart.Parent=nil;
+HumanoidRootPart.Parent = Character
