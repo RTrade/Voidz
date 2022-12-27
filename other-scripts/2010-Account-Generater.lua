f5fc0765095c557a9f65f94746eea5dfaeea7862
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-27 19:40:39 +0000

    Create 2010-Account-Generater.lua

---
+++ other-scripts/2010-Account-Generater.lua
@@ -0,0 +1,240 @@
+--[[
+Welcome to 2010 Roblox Account Generator
+
+This run's in Roblox Studio and execution software such as Synapse, Script-Ware, Krnl etc.
+
+This Bot can generate 100's of Roblox acccounts from 2010.
+
+Type in /console to View Generated Accounts
+
+You can also view the File of all Generated Accounts by opening the 'workspace' Folder on your Desktop ( if you have synapse ) and viewing Generated_Accounts.txt
+
+BY USING THIS SCRIPT YOU AGREE THAT THIS IS FOR EDUCATIONAL PURPOSES, ANY DAMAGE CAUSED IS NOT MY FAULT.
+]]
+local gen = {}
+table.insert(gen,[[
+   Account Generator by Trade#6676. rtrade.github.io
+   
+    ]])
+ table.insert(gen,[[
+      
+      ]])
+function check(acc)
+	if acc:match("1") or acc:match("2") or  acc:match("3") or  acc:match("4") or  acc:match("5") or  acc:match("6") or  acc:match("7") or  acc:match("8") or  acc:match("9") and not acc:match("roblox")  then
+		return true
+	else
+		return false
+	end
+end
+function crack()
+	return "l0l0l0l"
+end
+game:GetService("RunService").RenderStepped:Connect(function()
+	local bot = false
+	local generated = math.random(11603842,19999999)
+	local success,chr = pcall(function()
+		return game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)) and true or false
+	end)
+	if success then
+
+		status = check(game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)))
+		if status then
+			bot = true
+		end
+		end
+	if   success and bot then
+		local convertgen = game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated))
+if  string.len(convertgen) == 20   then
+		print("Generated Account: "..convertgen.." Password: "..crack())
+      local success,err = pcall(function()
+      table.insert(gen,"Generated Account: "..convertgen.." Password: "..crack()..[[
+      
+      ]])
+          if writefile then
+            
+            end
+          end)
+		end
+		end
+		
+end)
+game:GetService("RunService").RenderStepped:Connect(function()
+    wait(0.1)
+    if writefile then
+      writefile("Generated_Accounts.txt",table.concat(gen))
+      end
+    end)
+game:GetService("RunService").RenderStepped:Connect(function()
+	local bot = false
+	local generated = math.random(11603842,19999999)
+	local success,chr = pcall(function()
+		return game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)) and true or false
+	end)
+	if success then
+
+		status = check(game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)))
+		if status then
+			bot = true
+		end
+		end
+	if   success and bot then
+		local convertgen = game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated))
+if  string.len(convertgen) == 20   then
+		print("Generated Account: "..convertgen.." Password: "..crack())
+      local success,err = pcall(function()
+      table.insert(gen,"Generated Account: "..convertgen.." Password: "..crack()..[[
+      
+      ]])
+          if writefile then
+            
+            end
+          end)
+		end
+		end
+		
+end)
+game:GetService("RunService").RenderStepped:Connect(function()
+	local bot = false
+	local generated = math.random(11603842,19999999)
+	local success,chr = pcall(function()
+		return game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)) and true or false
+	end)
+	if success then
+
+		status = check(game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)))
+		if status then
+			bot = true
+		end
+		end
+	if   success and bot then
+		local convertgen = game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated))
+if  string.len(convertgen) == 20   then
+		print("Generated Account: "..convertgen.." Password: "..crack())
+      local success,err = pcall(function()
+      table.insert(gen,"Generated Account: "..convertgen.." Password: "..crack()..[[
+      
+      ]])
+          if writefile then
+            
+            end
+          end)
+		end
+		end
+		
+end)
+game:GetService("RunService").RenderStepped:Connect(function()
+	local bot = false
+	local generated = math.random(11603842,19999999)
+	local success,chr = pcall(function()
+		return game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)) and true or false
+	end)
+	if success then
+
+		status = check(game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)))
+		if status then
+			bot = true
+		end
+		end
+	if   success and bot then
+		local convertgen = game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated))
+if  string.len(convertgen) == 20   then
+		print("Generated Account: "..convertgen.." Password: "..crack())
+      local success,err = pcall(function()
+      table.insert(gen,"Generated Account: "..convertgen.." Password: "..crack()..[[
+      
+      ]])
+          if writefile then
+            
+            end
+          end)
+		end
+		end
+		
+end)
+game:GetService("RunService").RenderStepped:Connect(function()
+	local bot = false
+	local generated = math.random(11603842,19999999)
+	local success,chr = pcall(function()
+		return game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)) and true or false
+	end)
+	if success then
+
+		status = check(game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)))
+		if status then
+			bot = true
+		end
+		end
+	if   success and bot then
+		local convertgen = game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated))
+if  string.len(convertgen) == 20   then
+		print("Generated Account: "..convertgen.." Password: "..crack())
+      local success,err = pcall(function()
+      table.insert(gen,"Generated Account: "..convertgen.." Password: "..crack()..[[
+      
+      ]])
+          if writefile then
+            
+            end
+          end)
+		end
+		end
+		
+end)
+game:GetService("RunService").RenderStepped:Connect(function()
+	local bot = false
+	local generated = math.random(11603842,19999999)
+	local success,chr = pcall(function()
+		return game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)) and true or false
+	end)
+	if success then
+
+		status = check(game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)))
+		if status then
+			bot = true
+		end
+		end
+	if   success and bot then
+		local convertgen = game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated))
+if  string.len(convertgen) == 20   then
+		print("Generated Account: "..convertgen.." Password: "..crack())
+      local success,err = pcall(function()
+      table.insert(gen,"Generated Account: "..convertgen.." Password: "..crack()..[[
+      
+      ]])
+          if writefile then
+            writefile("Generated_Accounts.txt",table.concat(gen))
+            end
+          end)
+		end
+		end
+		
+end)
+game:GetService("RunService").RenderStepped:Connect(function()
+	local bot = false
+	local generated = math.random(11603842,19999999)
+	local success,chr = pcall(function()
+		return game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)) and true or false
+	end)
+	if success then
+
+		status = check(game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated)))
+		if status then
+			bot = true
+		end
+		end
+	if   success and bot then
+		local convertgen = game:GetService("Players"):GetNameFromUserIdAsync(tonumber(generated))
+if  string.len(convertgen) == 20   then
+		print("Generated Account: "..convertgen.." Password: "..crack())
+      local success,err = pcall(function()
+      table.insert(gen,"Generated Account: "..convertgen.." Password: "..crack()..[[
+      
+      ]])
+          if writefile then
+            
+            end
+          end)
+		end
+		end
+		
+end)
