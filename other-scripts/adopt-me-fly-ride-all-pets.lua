7a02f10e2fe037d9cadca25f0de88203a4f754ce
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-28 22:13:53 +0000

    Create adopt-me-fly-ride-all-pets.lua

---
+++ other-scripts/adopt-me-fly-ride-all-pets.lua
@@ -0,0 +1,14 @@
+-- omg i made an game for 6 year olds
+
+-- settings
+local Rideable = true -- Set to true to make all pets rideable or set to false to not make all pets rideable
+local Flyable = true -- Set to true to make all pets flyable or set to false to not make all pets flyable
+
+
+for i,pet in pairs(require(game.ReplicatedStorage.ClientModules.Core.ClientData).get_data()[tostring(game:GetService("Players").LocalPlayer)].inventory.pets) do
+  if Rideable then
+pet.properties.rideable = true
+    elseif Flyable then
+    pet.properties.flyable = true
+    end
+end
