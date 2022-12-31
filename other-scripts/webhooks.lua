436a3d77a13b7cb276e46338d975c98d89912944
RTrade <114867982+RTrade@users.noreply.github.com>
2022-12-31 12:47:05 +0000

    Update webhooks.lua

--- other-scripts/webhooks.lua
+++ other-scripts/webhooks.lua
@@ -1,7 +1,7 @@
 -- bro looked here then found out the code did nothing
 local encryptedwebhooks = {
  "إخوانه لماذا تحاول سرقة خطاف الويب الخاص بي؟",
-  "('encrypted by luacrypter') ('%£%*$££*££$£%£*$£$£')"
+  "btw i dont have a webhook dumbass"
   }
 
 for I_Hate,You in ipairs(encryptedwebhooks) do
