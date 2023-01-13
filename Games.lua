7b5d1376e46e13abdd4b837ab0f4fd49e0d11aa6
RTrade <114867982+RTrade@users.noreply.github.com>
2023-01-13 22:02:47 +0000

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -56,7 +56,9 @@ local Supported_Games = {
  ["6990464163"] = "Combat-Mayhem.lua",
  ["189707"] = "Natural%20Disaster%20Survival.lua",
  ["11656036986"] = "make%20roblox%20games%20to%20become%20rich%20and%20famous.lua",
- ["185655149"] = "Bloxburg.lua"
+ ["185655149"] = "Bloxburg.lua",
+ ["62291169342"] = "HOOPZ.lua",
+ ["164051105"] = "Super%20Bomb%20Survival.lua"
 }
 
 local success, err = pcall(function()
