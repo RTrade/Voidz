351726d35cd61a82145075d62a8860a6de487ff3
RTrade <114867982+RTrade@users.noreply.github.com>
2023-01-13 20:36:57 +0000

    Create medmenu.lua

---
+++ other-scripts/medmenu.lua
@@ -0,0 +1,242 @@
+--[[
+dude no way!1 i got epic mod menu for roblox
+if you do not get it alreday this entire file is an joke i made in like 5 mins then used gui to lua
+]]
+
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local om = Instance.new("ScreenGui")
+local omgMODMENU = Instance.new("Frame")
+local coolflyrealwork = Instance.new("TextButton")
+local esp1hackor111 = Instance.new("TextButton")
+local walkinwall1work100legit = Instance.new("TextButton")
+local banalluniversalwork1000001 = Instance.new("TextButton")
+local ROBLOXMODMENUCRACK = Instance.new("TextLabel")
+local CRACKEDBYME1legithackoriamcooooooooool = Instance.new("TextLabel")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+
+--Properties:
+
+om.Name = "omg crack 10000000000000000% work by timmygamer69"
+om.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+om.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+omgMODMENU.Name = "omgMODMENU"
+omgMODMENU.Parent = om
+omgMODMENU.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+omgMODMENU.Position = UDim2.new(0.0326864161, 0, 0.166666657, 0)
+omgMODMENU.Size = UDim2.new(0, 426, 0, 266)
+
+coolflyrealwork.Name = "cool fly real work"
+coolflyrealwork.Parent = omgMODMENU
+coolflyrealwork.BackgroundColor3 = Color3.fromRGB(255, 215, 93)
+coolflyrealwork.BackgroundTransparency = 0.100
+coolflyrealwork.Position = UDim2.new(0.0234741792, 0, 0.541353345, 0)
+coolflyrealwork.Size = UDim2.new(0, 200, 0, 50)
+coolflyrealwork.Font = Enum.Font.SourceSans
+coolflyrealwork.Text = "FLY HACK"
+coolflyrealwork.TextColor3 = Color3.fromRGB(25, 236, 255)
+coolflyrealwork.TextScaled = true
+coolflyrealwork.TextSize = 14.000
+coolflyrealwork.TextWrapped = true
+
+esp1hackor111.Name = "esp!1 hackor!111"
+esp1hackor111.Parent = omgMODMENU
+esp1hackor111.BackgroundColor3 = Color3.fromRGB(255, 215, 93)
+esp1hackor111.BackgroundTransparency = 0.100
+esp1hackor111.Position = UDim2.new(0.514084518, 0, 0.541353345, 0)
+esp1hackor111.Size = UDim2.new(0, 200, 0, 50)
+esp1hackor111.Font = Enum.Font.SourceSans
+esp1hackor111.Text = "ESP"
+esp1hackor111.TextColor3 = Color3.fromRGB(25, 236, 255)
+esp1hackor111.TextScaled = true
+esp1hackor111.TextSize = 14.000
+esp1hackor111.TextWrapped = true
+
+walkinwall1work100legit.Name = "walk in wall!1 work 100% legit"
+walkinwall1work100legit.Parent = omgMODMENU
+walkinwall1work100legit.BackgroundColor3 = Color3.fromRGB(255, 215, 93)
+walkinwall1work100legit.BackgroundTransparency = 0.100
+walkinwall1work100legit.Position = UDim2.new(0.0234741867, 0, 0.770676672, 0)
+walkinwall1work100legit.Size = UDim2.new(0, 200, 0, 50)
+walkinwall1work100legit.Font = Enum.Font.SourceSans
+walkinwall1work100legit.Text = "NOCLIP"
+walkinwall1work100legit.TextColor3 = Color3.fromRGB(25, 236, 255)
+walkinwall1work100legit.TextScaled = true
+walkinwall1work100legit.TextSize = 14.000
+walkinwall1work100legit.TextWrapped = true
+
+banalluniversalwork1000001.Name = "ban all universal work 100000%!1"
+banalluniversalwork1000001.Parent = omgMODMENU
+banalluniversalwork1000001.BackgroundColor3 = Color3.fromRGB(255, 215, 93)
+banalluniversalwork1000001.BackgroundTransparency = 0.100
+banalluniversalwork1000001.Position = UDim2.new(0.514084518, 0, 0.770676672, 0)
+banalluniversalwork1000001.Size = UDim2.new(0, 200, 0, 50)
+banalluniversalwork1000001.Font = Enum.Font.SourceSans
+banalluniversalwork1000001.Text = "BAN ALL"
+banalluniversalwork1000001.TextColor3 = Color3.fromRGB(25, 236, 255)
+banalluniversalwork1000001.TextScaled = true
+banalluniversalwork1000001.TextSize = 14.000
+banalluniversalwork1000001.TextWrapped = true
+
+ROBLOXMODMENUCRACK.Name = "ROBLOX MOD MENU CRACK"
+ROBLOXMODMENUCRACK.Parent = omgMODMENU
+ROBLOXMODMENUCRACK.BackgroundColor3 = Color3.fromRGB(190, 190, 190)
+ROBLOXMODMENUCRACK.BackgroundTransparency = 0.100
+ROBLOXMODMENUCRACK.BorderSizePixel = 0
+ROBLOXMODMENUCRACK.Size = UDim2.new(0, 426, 0, 50)
+ROBLOXMODMENUCRACK.Font = Enum.Font.SourceSans
+ROBLOXMODMENUCRACK.Text = "ROBLOX MOD MENU CRACKED"
+ROBLOXMODMENUCRACK.TextColor3 = Color3.fromRGB(22, 22, 22)
+ROBLOXMODMENUCRACK.TextScaled = true
+ROBLOXMODMENUCRACK.TextSize = 14.000
+ROBLOXMODMENUCRACK.TextWrapped = true
+
+CRACKEDBYME1legithackoriamcooooooooool.Name = "CRACKED BY ME!1 legit hackor i am cooooooooool"
+CRACKEDBYME1legithackoriamcooooooooool.Parent = omgMODMENU
+CRACKEDBYME1legithackoriamcooooooooool.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CRACKEDBYME1legithackoriamcooooooooool.Position = UDim2.new(0.150234729, 0, 0.165413529, 0)
+CRACKEDBYME1legithackoriamcooooooooool.Size = UDim2.new(0, 297, 0, 50)
+CRACKEDBYME1legithackoriamcooooooooool.Font = Enum.Font.SourceSans
+CRACKEDBYME1legithackoriamcooooooooool.Text = "cracked by timmygamer69"
+CRACKEDBYME1legithackoriamcooooooooool.TextColor3 = Color3.fromRGB(0, 0, 0)
+CRACKEDBYME1legithackoriamcooooooooool.TextScaled = true
+CRACKEDBYME1legithackoriamcooooooooool.TextSize = 14.000
+CRACKEDBYME1legithackoriamcooooooooool.TextWrapped = true
+
+Frame.Parent = om
+Frame.BackgroundColor3 = Color3.fromRGB(86, 86, 86)
+Frame.BackgroundTransparency = 0.500
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.484167516, 0, 0.333333313, 0)
+Frame.Size = UDim2.new(0, 404, 0, 100)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(0.152709961, 0, -0.00458648661, 0)
+TextLabel.Size = UDim2.new(0, 297, 0, 25)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "mod menu legit 1000000000%"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.Position = UDim2.new(0.152709961, 0, 0.535413504, 0)
+TextLabel_2.Size = UDim2.new(0, 297, 0, 25)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "loading 69% complete!1"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+-- Scripts:
+
+local function XLVV_fake_script() -- CRACKEDBYME1legithackoriamcooooooooool.RainbowBackgroundScript 
+	local script = Instance.new('LocalScript', CRACKEDBYME1legithackoriamcooooooooool)
+
+	function zigzag(X) return math.acos(math.cos(X*math.pi))/math.pi end
+	
+	counter = 0
+	
+	while wait(0.001)do
+	 script.Parent.BackgroundColor3 = Color3.fromHSV(zigzag(counter),1,1)
+	 
+	 counter = counter + 0.01
+	end
+end
+coroutine.wrap(XLVV_fake_script)()
+local function VBGVF_fake_script() -- omgMODMENU.mod hack epic drag!1 
+	local script = Instance.new('LocalScript', omgMODMENU)
+
+	local UIS = game:GetService("UserInputService")
+	function dragify(Frame)
+	    dragToggle = nil
+	    local dragSpeed = 0.50
+	    dragInput = nil
+	    dragStart = nil
+	    local dragPos = nil
+	    function updateInput(input)
+	        local Delta = input.Position - dragStart
+	        local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+	        game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.30), {Position = Position}):Play()
+	    end
+	    Frame.InputBegan:Connect(function(input)
+	        if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+	            dragToggle = true
+	            dragStart = input.Position
+	            startPos = Frame.Position
+	            input.Changed:Connect(function()
+	                if input.UserInputState == Enum.UserInputState.End then
+	                    dragToggle = false
+	                end
+	            end)
+	        end
+	    end)
+	    Frame.InputChanged:Connect(function(input)
+	        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+	            dragInput = input
+	        end
+	    end)
+	    game:GetService("UserInputService").InputChanged:Connect(function(input)
+	        if input == dragInput and dragToggle then
+	            updateInput(input)
+	        end
+	    end)
+	end
+	
+	dragify(script.Parent)
+end
+coroutine.wrap(VBGVF_fake_script)()
+local function RZHS_fake_script() -- TextLabel.RainbowBackgroundScript 
+	local script = Instance.new('LocalScript', TextLabel)
+
+	function zigzag(X) return math.acos(math.cos(X*math.pi))/math.pi end
+	
+	counter = 0
+	
+	while wait(0.001)do
+	 script.Parent.BackgroundColor3 = Color3.fromHSV(zigzag(counter),1,1)
+	 
+	 counter = counter + 0.01
+	end
+end
+coroutine.wrap(RZHS_fake_script)()
+local function NSRNGS_fake_script() -- TextLabel_2.RainbowBackgroundScript 
+	local script = Instance.new('LocalScript', TextLabel_2)
+
+	function zigzag(X) return math.acos(math.cos(X*math.pi))/math.pi end
+	
+	counter = 0
+	
+	while wait(0.001)do
+	 script.Parent.BackgroundColor3 = Color3.fromHSV(zigzag(counter),1,1)
+	 
+	 counter = counter + 0.01
+	end
+end
+coroutine.wrap(NSRNGS_fake_script)()
+local function NYKV_fake_script() -- Frame.LocalScript 
+	local script = Instance.new('LocalScript', Frame)
+
+	wait(2)
+	print("epic mod menu cracked by timmygamer69 loadded suksessfukl!1")
+	script.Parent.Parent.omgMODMENU.Visible = true
+	for i,v in pairs(script.Parent.Parent.omgMODMENU) do
+		if v:IsA("TextButton")then
+			v.MoseButton1Click:Connect(function()
+				game:GetService("Players").LocalPlayer:Kick("omg mod hack real 100% not work right now on "..string.lower(v.Text).." idk why but mod hack 100% legit!1")
+			end)
+		end
+	end
+	script.Parent.Visible = false
+end
+coroutine.wrap(NYKV_fake_script)()
