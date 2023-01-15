2024c4810411623f91bf13d13878da8e9eb88650
RTrade <114867982+RTrade@users.noreply.github.com>
2023-01-15 12:06:55 +0000

    Create VM_Main.lua

---
+++ Lua-VM/VM_Main.lua
@@ -0,0 +1,15 @@
+--[[
+Lua-VM is an simple Lua bytecode obfusctor created by RTrade, For the perpose of obfuscating code securely.
+Any Attempts of Tampering, Reverse Enginering or Deobfuscation will result in a blacklist from any scripts protected by Lua-VM.
+
+Goto rtrade.github.io to find out how Lua-VM and Voidz work.
+
+
+█████████████████████████████████████████
+█▄─▄███▄─██─▄██▀▄─██▀▀▀▀▀██▄─█─▄█▄─▀█▀─▄█
+██─██▀██─██─███─▀─██████████▄▀▄███─█▄█─██
+▀▄▄▄▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀▀▀▀▀▀▀▀▀▀▄▀▀▀▄▄▄▀▄▄▄▀
+█████████████████████████████████████████
+
+
+]]
